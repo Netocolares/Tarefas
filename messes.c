b1a2baf801c5e600de899c99f405c4102fc1cada
Netocolares <138890264+Netocolares@users.noreply.github.com>
2023-07-07 10:47:49 -0400

    Create messes.c

---
+++ messes.c
@@ -0,0 +1,54 @@
+  #include <stdio.h>
+
+int main(void){
+
+    int dias;
+
+ 
+    printf("digite um numero que esteja entre 1 e 12\n");
+
+        scanf("%d", &dias);
+
+    switch (dias)
+    {
+    case 1:
+        printf("janeiro");
+        break;
+    case 2:
+        printf("fevereiro");
+        break;
+    case 3:
+        printf("marco");
+        break;
+    case 4:
+        printf("abril");
+        break;
+    case 5:
+        printf("maio");
+        break;
+    case 6:
+        printf("junho");
+        break;
+    case 7:
+        printf("julho");
+        break;
+    case 8:
+        printf("agosto");
+        break;
+    case 9:
+        printf("setembro");
+        break;
+    case 10:
+        printf("outubro");
+        break;
+    case 11:
+        printf("novembro");
+        break;
+    case 12:
+        printf("dezembro");
+        break;
+    default:
+        break;
+    }
+
+    return 0;
